6126a0a45e16170fca1bb5aa60fe8105ddcd55fc
EinGitHub <137085448+EinGitHub@users.noreply.github.com>
2023-06-20 22:46:07 -0400

    First update

---
+++ mp8.java
@@ -0,0 +1,15 @@
+package Class2;
+
+public class mp8 
+
+{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myrandom = (int) (Math.random()*101);
+		System.out.println(myrandom);
+
+	}
+
+}
+
